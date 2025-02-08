a28a260b44686233b9928f175669ef089c97b62d
Panagiotis Prattis <panagiotispr1997@gmail.com>
2025-02-09 00:54:02 +0200

    header & form components

---
+++ src/app/user-input/user-input.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import {FormsModule} from "@angular/forms";
+
+@Component({
+  selector: 'app-user-input',
+  imports: [
+    FormsModule
+  ],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css'
+})
+export class UserInputComponent {
+
+  onSubmit() {
+
+  }
+}
